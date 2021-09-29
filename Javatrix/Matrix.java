ed139bc885882cba6deafcdab5cf43a5480d1654
Curt Bridgers <bridgerspc@appstate.edu>
2021-09-29 13:23:43 -0400

    Added 2 constructors, matrix double[][] field, and getArray accessor for that field. No tests yet.

--- Javatrix/Matrix.java
+++ Javatrix/Matrix.java
@@ -10,8 +10,24 @@ package Javatrix;
 public class Matrix
 {
     // Fields
+    private double[][] matrix;
     
     // Constructors
+    public Matrix(double[][] A) {
+        this.matrix = A;
+    }
+
+    public Matrix(int m, int n, double s) {
+        this.matrix = new double[m][n];
+        for (int row = 0; row < m; row++) {
+           for (int col = 0; col < n; col++) {
+              this.matrix[row][col] = s;
+           }
+        }
+    }
 
     // Methods
+    public double[][] getArray() {
+      return this.matrix;
+    }
 }
