ca720b477e15e0721c7f47e54177a931f8e87e88
Curt Bridgers <bridgerspc@appstate.edu>
2021-10-03 23:56:48 -0400

    Added pretty printer and pretty printer test

--- Javatrix/Matrix.java
+++ Javatrix/Matrix.java
@@ -30,4 +30,21 @@ public class Matrix
     public double[][] getArray() {
       return this.matrix;
     }
+
+    public void print(int w, int d) {
+      String fStr = String.format("%%%d.%df  ", w, d);
+      for (int row = 0; row < matrix.length; row++) {
+         System.out.printf("[ ");
+         for (int col = 0; col < matrix[row].length; col++) {
+            System.out.printf(fStr, matrix[row][col]);
+         }
+         System.out.printf(" ]");
+         System.out.println();
+      }
+    }
+
+    public static void main(String[] args) {
+        Matrix m = new Matrix(3,3,5);
+        m.print(5, 3);
+    }
 }
